895426cb4a790958bb79973d59677d68333c199e
Cristhian Forero <cristhianfdx@gmail.com>
2020-11-08 16:33:30 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'sire-client-app';
-}
+export class AppComponent {}
